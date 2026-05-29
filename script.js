c80229ace17da6219b122a1cbfe4e3824e9c031a
garagegroundsound <garagegroundsound@gmail.com>
2026-05-29 17:05:21 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', () => {
-  const scroll = window.scrollY;
-  const bg = document.querySelector('.background');
-  if(bg){
-    bg.style.transform = `translateY(${scroll * 0.1}px)`;
-  }
-});
